fe4b7b7073fe5667a2eea27281498c213c9678ee
Zach Lipp <zlipp@protonmail.com>
2024-03-26 17:34:05 -0400

    debugged with netlify cli, cool stuff

---
+++ functions/submission-created.mjs
@@ -0,0 +1,24 @@
+/* https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget/ */
+export const handler = async event => {
+  const email = JSON.parse(event.body).payload.email
+  return fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${ process.env.BUTTONDOWN_KEY }`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then(response => {
+      if (!response.ok) {
+            throw new Error('Network response was not ok');
+         }
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      return { statusCode: 200 }
+    })
+    //.then({statusCode: response.status, body: response.text()})
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+}
