8690783f8dd0b4e5584192b993c5534359ddf5df
Zach Lipp <zlipp@protonmail.com>
2024-03-26 14:23:08 -0400

    one more

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,4 @@
 /* https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget/ */
-const fetch = require('node-fetch')
 exports.handler = async event => {
   const email = JSON.parse(event.body).payload.email
   console.log(`Recieved a submission: ${email}`)
