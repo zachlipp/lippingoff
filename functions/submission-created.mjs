0894933ad329837ebce16d856f177c96b528935e
Zach Lipp <zlipp@protonmail.com>
2024-03-27 14:13:51 -0400

    okay this would actually make sense if it fixes things

--- functions/submission-created.mjs
+++ functions/submission-created.mjs
@@ -8,6 +8,7 @@ export const handler = async event => {
       Authorization: `Token ${ process.env.BUTTONDOWN_KEY }`,
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST",
     },
     body: JSON.stringify({ email }),
