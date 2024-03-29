6fa35d5642e928a5b5bc7432dd7b14317f9470e3
Zach Lipp <zlipp@protonmail.com>
2024-03-29 15:32:11 -0400

    another try

--- functions/submission-created.mjs
+++ functions/submission-created.mjs
@@ -8,8 +8,8 @@ export const handler = async event => {
       Authorization: `Token ${ process.env.BUTTONDOWN_KEY }`,
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST",
+      "Access-Control-Allow-Headers": "Content-Type, Origin",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     },
     body: JSON.stringify({ email }),
   })
